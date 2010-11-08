0f284dd35200de6551faff547107090700b3a948
sbeam <sbeam@onsetcorps.net>
2010-11-07 20:59:25 -0500

    wip ajax subscribe from footer

--- mail_chimp_hooks.rb
+++ mail_chimp_hooks.rb
@@ -3,7 +3,10 @@ class MailChimpHooks < Spree::ThemeSupport::HookListener
 
   insert_after :signup_below_password_fields, 'subscribe_to_newsletter_field'
 
-  
+  # Add js for ajaxy subscriptions from footer or wherever
+  insert_after :inside_head do
+    "<%= javascript_include_tag 'mailchimp_subscribe' %>"
+  end
 
 
 end
--- public/javascripts/mailchimp_subscribe.js
+++ public/javascripts/mailchimp_subscribe.js
@@ -1,1 +1,20 @@
-alert('ofof');
+alert('AAAAN~');
+
+var subscribe_email_default_txt = '';
+
+jQuery(document).ready( function() {
+
+    subscribe_email_default_txt = $('#subscribe_email').val();
+
+    $('#subscribe_email').bind('focus', function() {
+      if (this.value == subscribe_email_default_txt) this.value = '';
+    });
+    $('#subscribe_email').bind('blur', function() {
+      if (this.value == '') this.value = subscribe_email_default_txt;
+    });
+
+
+    $('input[type=text],input[type=password]').addClass('textfield').wrap('<span class="inputborderwrap"></span>');
+
+
+});

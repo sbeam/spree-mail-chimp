e16fa0508420d3a95a496be67b0e9c1775c0d282
sbeam <sbeam@onsetcorps.net>
2010-11-08 16:58:52 -0500

    correct path to partial for the subscribe checkbox

--- mail_chimp_hooks.rb
+++ mail_chimp_hooks.rb
@@ -1,7 +1,7 @@
 class MailChimpHooks < Spree::ThemeSupport::HookListener
 
 
-  insert_after :signup_below_password_fields, 'subscribe_to_newsletter_field'
+  insert_after :signup_below_password_fields, 'users/subscribe_to_newsletter_field'
 
   # Add js for ajaxy subscriptions from footer or wherever
   insert_after :inside_head do

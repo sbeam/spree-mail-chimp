90bcb574173f0a7374bf19a241dc8142710f852f
bzt <bastian.schnurle@silberstein.de>
2010-11-28 20:51:10 +0100

    spree_mail_chimp

--- config/initializers/mail_chimp_settings.rb
+++ config/initializers/mail_chimp_settings.rb
@@ -1,6 +1,6 @@
 if Spree::Config.instance 
-    Spree::Config.set(:mailchimp_api_key => '22c511cd11f94a3a65a542e3fd419aef-us2')
-    Spree::Config.set(:mailchimp_list_id => 'ece35ed568')
+    Spree::Config.set(:mailchimp_api_key => '')
+    Spree::Config.set(:mailchimp_list_id => '')
     Spree::Config.set(:mailchimp_subscription_opts => {:email_type => 'html', :secure => true, :double_opt_in => true})
 end 
 

907c00a3acecd13380ee138af3db59524b0a91a0
REMI GAGNON <remigagnon@remi-gagnons-macbook-pro.local>
2011-04-30 12:50:32 -0400

    refactoring

---
+++ lib/spree_mail_chimp.rb
@@ -0,0 +1,35 @@
+require 'spree_core'
+require 'spree_mail_chimp_hooks'
+require 'Spree_mail_chimp_sync'
+require 'hominid'
+module SpreeMailChimp
+  class Engine < Rails::Engine
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    def self.activate
+
+      Spree::BaseController.class_eval do
+        helper MailChimpHelper
+      end
+
+      UsersController.send(:include, MailChimpSync::Sync)
+
+      User.class_eval do
+        attr_accessible :is_mail_list_subscriber
+      end
+
+      AppConfiguration.class_eval do
+        preference :mailchimp_double_opt_in, :boolean, :default => false
+        preference :mailchimp_send_welcome, :boolean, :default => false
+        preference :mailchimp_send_notify, :boolean, :default => false
+        preference :mailchimp_merge_vars, :string, :default => ''
+        preference :mailchimp_list_id, :string
+        preference :mailchimp_api_key, :string
+      end
+
+    end
+
+    config.to_prepare &method(:activate).to_proc
+  end
+end

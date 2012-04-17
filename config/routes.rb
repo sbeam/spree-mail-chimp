bc22601b3de767ef3c8b993231bdead1eef20b01
James Kong <jkongie@gmail.com>
2012-04-17 17:58:33 +0800

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-    resources :subscriptions, :only => :create
+  resources :subscriptions, :only => :create
 
-    namespace :admin do
-        resource  :mail_chimp_settings
-    end  
+  namespace :admin do
+    resource  :mail_chimp_settings
+  end
 
 end

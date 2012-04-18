095bb4b55b0c38373720bd9028b966923dc6236b
James Kong <jkongie@gmail.com>
2012-04-18 11:31:02 +0800

    Update migrations to use the Spree 1.0 column naming

--- db/migrate/20101102200125_add_subscriber_column_to_users.rb
+++ db/migrate/20101102200125_add_subscriber_column_to_users.rb
@@ -1,9 +1,9 @@
 class AddSubscriberColumnToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :is_mail_list_subscriber, :boolean, :null => false, :default => false
+    add_column :spree_users, :is_mail_list_subscriber, :boolean, :null => false, :default => false
   end
 
   def self.down
-    remove_column :users, :is_mail_list_subscriber
+    remove_column :spree_users, :is_mail_list_subscriber
   end
 end
--- db/migrate/20101103020507_add_smc_member_id_to_users.rb
+++ db/migrate/20101103020507_add_smc_member_id_to_users.rb
@@ -1,9 +1,9 @@
 class AddSmcMemberIdToUsers < ActiveRecord::Migration
   def self.up
-    add_column :users, :mailchimp_subscriber_id, :string, :length => 31
+    add_column :spree_users, :mailchimp_subscriber_id, :string, :length => 31
   end
 
   def self.down
-    remove_column :users, :mailchimp_subscriber_id
+    remove_column :spree_users, :mailchimp_subscriber_id
   end
 end

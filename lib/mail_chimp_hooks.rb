f515ed89e0e11e08574b48eaf831f6d4fad41840
James Kong <jkongie@gmail.com>
2012-04-18 12:54:39 +0800

    These hooks are deprecated! Use overrides instead

--- lib/mail_chimp_hooks.rb
+++ lib/mail_chimp_hooks.rb
@@ -1,20 +1,19 @@
-class MailChimpHooks < Spree::ThemeSupport::HookListener
-	insert_after :signup_below_password_fields, 'users/subscribe_to_newsletter_field'
-	insert_after :footer_left, 'shared/newsletter_subscribe_form'
-
-	# Add js for ajaxy subscriptions from footer or wherever
-	insert_after :inside_head do
-        "<%= stylesheet_link_tag 'mail_chimp' %>
-		<%= javascript_include_tag 'jquery.simplemodal.1.4.min.js','mailchimp_subscribe' %>"
-    end
-
-    insert_after :admin_configurations_menu do
-        %[
-          <tr>
-            <td><%= link_to t("mailchimp_settings"), admin_mail_chimp_settings_path %></td>
-            <td><%= t("mailchimp_settings_description") %></td>
-          </tr>
-        ] 
-    end
-
-end
+# class MailChimpHooks < Spree::ThemeSupport::HookListener
+#   insert_after :footer_left, 'shared/newsletter_subscribe_form'
+#
+#   # Add js for ajaxy subscriptions from footer or wherever
+#   insert_after :inside_head do
+#         "<%= stylesheet_link_tag 'mail_chimp' %>
+#     <%= javascript_include_tag 'jquery.simplemodal.1.4.min.js','mailchimp_subscribe' %>"
+#     end
+#
+#     insert_after :admin_configurations_menu do
+#         %[
+#           <tr>
+#             <td><%= link_to t("mailchimp_settings"), admin_mail_chimp_settings_path %></td>
+#             <td><%= t("mailchimp_settings_description") %></td>
+#           </tr>
+#         ]
+#     end
+#
+# end

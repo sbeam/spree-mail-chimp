42127ed239aea0f246db4e996cf9aa5a4a8bc273
James Kong <jkongie@gmail.com>
2012-04-19 10:23:40 +0800

    Convert the code from the MailChimpSync module into a decorator. Still needs more work!

--- app/controllers/spree/subscriptions_controller.rb
+++ app/controllers/spree/subscriptions_controller.rb
@@ -26,7 +26,7 @@ class Spree::SubscriptionsController < Spree::BaseController
           self.class.benchmark "Adding mailchimp subscriber" do
             hominid.subscribe(Spree::Config.get(:mailchimp_list_id), params[:email], {}, MailChimpSync::Sync::mc_subscription_opts)
           end
-          rescue Hominid::ValidationError => e
+        rescue Hominid::ValidationError => e
           @errors << t('invalid_email_address')
         end
       end

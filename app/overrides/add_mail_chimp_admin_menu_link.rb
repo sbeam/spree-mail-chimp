7e3d19d7346d76c3082b5d2bcd4e429cd0123583
chuckguynn <chuckguynn@me.com>
2013-06-10 07:35:28 -0500

    update override for spree 1.3

--- app/overrides/add_mail_chimp_admin_menu_link.rb
+++ app/overrides/add_mail_chimp_admin_menu_link.rb
@@ -1,4 +1,6 @@
-Deface::Override.new(:virtual_path  => 'spree/admin/configurations/index',
+Deface::Override.new(:virtual_path  => 'spree/admin/shared/_configuration_menu',
                      :name          => 'add_mail_chimp_admin_menu_link',
-                     :insert_bottom => "[data-hook='admin_configurations_menu']",
-                     :partial       => 'spree/admin/configurations/spree_mail_chimp_configuration_link' )
+                     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+                    :text => %q{
+                        <%= configurations_sidebar_menu_item 'Mail Chimp', admin_mail_chimp_settings_path %>
+                     }) 
